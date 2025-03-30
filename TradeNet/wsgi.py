6e90058a7d5088bb0c8233fc42859cdf133fd1ad
ShivanshKansal <shivanshkansal19@gmail.com>
2025-03-30 21:33:46 +0530

    change project name

--- TradeNet/wsgi.py
+++ TradeNet/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Trading project.
+WSGI config for TradeNet project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Trading.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TradeNet.settings')
 
 application = get_wsgi_application()
