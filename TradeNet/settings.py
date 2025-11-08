43e6fc56bde30e49606032fcbe3327db27e2457b
ShivanshKansal <shivanshkansal19@gmail.com>
2025-11-09 01:28:51 +0530

    fix: fixed static file settings

--- TradeNet/settings.py
+++ TradeNet/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$xpgdyqt_a5^o6vl((-mj*4+ljcyys$+cv!r9v+oz%0flu#g7a'
+if os.getenv('SECRET_KEY'):
+    SECRET_KEY = os.getenv('SECRET_KEY')
+else:
+    SECRET_KEY = 'django-insecure-$xpgdyqt_a5^o6vl((-mj*4+ljcyys$+cv!r9v+oz%0flu#g7a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -133,11 +136,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
